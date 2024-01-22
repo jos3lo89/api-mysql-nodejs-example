1c40df80e2b3e30e7f666c8907a5a737cc82d7bd
jos3lo89 <gothangel21@hotmail.com>
2024-01-21 22:58:23 -0500

    funka22

--- src/app.js
+++ src/app.js
@@ -7,15 +7,15 @@ const app = express()
 
 app.use(express.json())
 
-app.use((req, res, next) => {
-    res.status(404).json({
-        mensaje: "endpoint no valido"
-    })
-})
 
 
 app.use(indexRoutes)
 app.use('/api', employeesRoutes)
+app.use((req, res, next) => {
+    res.status(404).json({
+        mensaje: "endpoint no valiwqeqwdo"
+    })
+})
 
 
 
